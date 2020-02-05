3a8141a9fd4c0c16b26940c960b1f5478de6cd84
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-02-05 07:21:14 +0100

    Read Fastq func

---
+++ reading_fastq.py
@@ -0,0 +1,21 @@
+def readFastq(filename):
+    qualities = []
+    sequences = []
+    with open(filename, 'r') as f:
+        while True:
+            ## Reads the first line (corresponds to tag of seq, we don't care about it)
+            f.readline();
+            # Reads second line : Our read
+            seq = f.readline().rstrip()
+            # Just a placeholder line in the fastq file
+            f.readline()
+            # Quality bases line
+            qual = f.readline().rstrip()
+            if len(seq) == 0:
+                break
+            sequences.append(seq)
+            qualities.append(qual)
+
+    return sequences, qualities
+
+print(readFastq('SRR835775_1.first1000.fastq'))
